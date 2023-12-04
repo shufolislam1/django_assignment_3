1624b14504b3a7cdbb797411bf0f7551184d454f
shufolislam1 <shufolislam1255@gmail.com>
2023-12-04 16:15:12 +0600

    50% of the project is done

--- about_us/views.py
+++ about_us/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def aboutUs(request):
+    return render(request, 'aboutUs.html')
--- resturant/urls.py
+++ resturant/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('meal/', include('meal.urls')),
+    path('about_us/', include('about_us.urls')),
+    path('', views.home)
 ]
